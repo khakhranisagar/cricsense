7e4cea5593b404e4510b24ab03fd7f89ad979903
sagar.khakhrani <14mca27@gmail.com>
2017-04-19 19:12:19 +0530

    Changed in demo

--- app/src/main/java/sagarkhakhrani/cricsense/Demo.java
+++ app/src/main/java/sagarkhakhrani/cricsense/Demo.java
@@ -9,5 +9,6 @@ public class Demo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
+        //das
     }
 }
